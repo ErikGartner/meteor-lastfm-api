614ff8c96d631e5b40b7026453a1c0359f749906
Erik <xxxsmoizxxx@gmail.com>
2015-12-11 12:16:26 +0100

    fix: Incorrect export

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'smoiz:lastfm-api',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'A Meteor package wrapper for the node package lastfm.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/ErikGartner/meteor-lastfm-api',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('lastfm-api.js');
+  api.addFiles('lastfm-api.js', 'server');
   api.export('LastFmNode', 'server');
 });
 
